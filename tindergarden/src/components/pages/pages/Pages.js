0cab1029c469867de3b1b312fe2af96a8aa066e5
Alireza Talaei <alireza@Alirezas-MacBook-Pro.local>
2022-05-03 10:31:06 +1000

    basic functionality works

--- tindergarden/src/components/pages/pages/Pages.js
+++ tindergarden/src/components/pages/pages/Pages.js
@@ -1,25 +1,71 @@
 import React, { Component } from "react";
-import {Routes, Route, Link} from "react-router-dom"
+import {BrowserRouter as Router, Switch, Redirect, Route, Link} from "react-router-dom"
 import Home from "../home/Home";
 import SignUp from "../../RegoPage/SignUp";
 import Edit from "../edit/Edit";
 import Profile from "../profile/Profile"
 import Login from "../../RegoPage/Login";
 import Post from "../newpost/Post";
+import PrivateDir from "../../PrivateDir";
+import PublicDir from "../../PublicDir";
+import {auth} from "../../../Firebase/firebase";
+import { Layout } from 'antd';
+import Searchbar from "../../searchbar/Searchbar";
+
+
+const {Content} = Layout;
 
 class Pages extends Component {
+    constructor() {
+        super();
+        this.state ={
+            authenticated: false,
+            loading: true,
+        };
+    }
+
+    componentDidMount() {
+        auth().onAuthStateChanged((user) => {
+          if (user) {
+            this.setState({
+              authenticated: true,
+              loading: false,
+            });
+          } else {
+            this.setState({
+              authenticated: false,
+              loading: false,
+            });
+          }
+        })
+      }
+
     render() {
-        return (
-            <div>
-                <Routes>
-                    <Route path="/home" element={<Home />} />
-                    <Route path="/signup" element={<SignUp />} />
-                    <Route path="/edit" element={<Edit />} />
-                    <Route path="/profile" element={<Profile />} />
-                    <Route path="/" element={<Login />} />
-                    <Route path="/newpost" element={<Post />} />
-                </Routes>
-            </div>
+        return this.state.loading === true ? <h2>Loading...</h2> : (
+            <Layout>
+                <Router>
+                    <Searchbar isLoggedIn={this.state.authenticated} />
+                    <Content>
+                    <div>
+                        <Switch>
+                            <PrivateDir exact path="/home" authenticated={this.state.authenticated} component={<Home />} /><PrivateDir/>                            
+                            <PrivateDir path="/edit" authenticated={this.state.authenticated} component={<Edit />} /><PrivateDir/>
+                            <PrivateDir path="/profile" authenticated={this.state.authenticated} component={<Profile />} /><PrivateDir/>
+                            <PrivateDir path="/" authenticated={this.state.authenticated} component={<Login />} /><PrivateDir/>
+                            <PrivateDir path="/newpost" authenticated={this.state.authenticated} component={<Post />} /><PrivateDir/>
+                            <PublicDir path="/login" authenticated={this.state.authenticated} component={<Login/>} /><PublicDir/>
+                            <PublicDir path="/signup" authenticated={this.state.authenticated} component={<SignUp />} /> <PublicDir/>
+                        </Switch>
+                    </div>
+                        
+                    </Content>                  
+                    
+                        
+                                       
+                </Router>
+            </Layout>
+                
+            
         )
 
     }
