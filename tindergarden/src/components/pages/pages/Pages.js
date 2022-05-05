23c27c328d18b42c2447a217fe365f97659023aa
Alireza Talaei <alireza@Alirezas-MacBook-Pro.local>
2022-05-05 15:19:11 +1000

    merged left

--- tindergarden/src/components/pages/pages/Pages.js
+++ tindergarden/src/components/pages/pages/Pages.js
@@ -12,7 +12,7 @@ import Searchbar from "../../searchbar/Searchbar";
 import PrivateDir from "../../PrivateDir";
 import PublicDir from "../../PublicDir";
 
-import pages from "./pages.css"
+import pages from "./Pages.css"
 
 const {Content} = Layout
 
